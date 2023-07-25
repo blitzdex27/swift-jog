0d0e85edb2931886418adbb7c80780fb650b4c3b
dexter ramos <blitzdex27@gmail.com>
2023-07-25 23:48:11 +0800

    update

---
+++ SwiftJog/Jog/Generics.swift
@@ -0,0 +1,8 @@
+//
+//  Generics.swift
+//  SwiftJog
+//
+//  Created by Dexter Ramos on 7/25/23.
+//
+
+import Foundation
---
+++ SwiftJogTests/GenericsTests.swift
@@ -0,0 +1,14 @@
+//
+//  GenericsTests.swift
+//  SwiftJogTests
+//
+//  Created by Dexter Ramos on 7/25/23.
+//
+
+import XCTest
+
+final class GenericsTests: XCTestCase {
+
+   
+
+}
