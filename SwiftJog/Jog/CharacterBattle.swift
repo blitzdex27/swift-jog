dda1343d9bc775f33f359e2b38e72206bb8a9a6a
dexter ramos <blitzdex27@gmail.com>
2023-07-25 22:50:28 +0800

    character battle

--- SwiftJog/Jog/CharacterBattle.swift
+++ SwiftJog/Jog/CharacterBattle.swift
@@ -27,3 +27,81 @@
 
 import Foundation
 
+enum GameCharacterType {
+    case elf
+    case giant
+    case wizard
+}
+
+protocol GameCharacter: AnyObject {
+    var name: String { get }
+    var hitPoints: Int { get set }
+    var attackPoints: Int { get }
+    
+    func strike(character: GameCharacter) -> Bool
+}
+
+extension GameCharacter {
+    func strike(character: GameCharacter) -> Bool {
+        character.hitPoints -= self.attackPoints
+        if character.hitPoints <= 0 {
+            return true
+        }
+        return false
+    }
+}
+
+struct GameCharacterFactory {
+    static func make(ofType: GameCharacterType) -> GameCharacter {
+        let characterType = ofType
+        switch characterType {
+        case .elf:
+            return Elf()
+        case .giant:
+            return Giant()
+        case .wizard:
+            return Wizard()
+        }
+    }
+}
+
+class Elf: GameCharacter {
+    var name: String = "Elf\(UUID().uuidString)"
+    var hitPoints: Int = 3
+    var attackPoints: Int = 10
+}
+
+class Giant: GameCharacter {
+    var name: String = "Giant\(UUID().uuidString)"
+    var hitPoints: Int = 10
+    var attackPoints: Int = 3
+}
+
+class Wizard: GameCharacter {
+    var name: String = "Wizard\(UUID().uuidString)"
+    var hitPoints: Int = 5
+    var attackPoints: Int = 5
+}
+
+func battle(_ character1: GameCharacter, vs character2: GameCharacter) {
+    
+    var striker1 = character1
+    var striker2 = character2
+    
+    
+    while true {
+        let didWin = striker1.strike(character: striker2)
+        
+        if didWin == true {
+            print("\(striker2.name) defeated!")
+            break
+        }
+        
+        // swap striker
+        let temp = striker1
+        striker1 = striker2
+        striker2 = temp
+    }
+}
+
+
--- SwiftJogTests/CharacterBattleTests.swift
+++ SwiftJogTests/CharacterBattleTests.swift
@@ -6,30 +6,112 @@
 //
 
 import XCTest
+@testable import SwiftJog
 
 final class CharacterBattleTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    let regexElf = { try Regex<AnyRegexOutput>("Elf") }
+    let regexGiant =  { try Regex<AnyRegexOutput>("Giant") }
+    let regexWizard = { try Regex<AnyRegexOutput>("Wizard") }
+    
+    let kStats_Elf = (hp: 3, atk: 10)
+    let kStats_Wizard = (hp: 5, atk: 5)
+    let kStats_Giant = (hp: 10, atk: 3)
+    
+    var elf = GameCharacterFactory.make(ofType: .elf)
+    var wizard = GameCharacterFactory.make(ofType: .wizard)
+    var giant = GameCharacterFactory.make(ofType: .giant)
+    
+    public override func setUp() {
+        elf = GameCharacterFactory.make(ofType: .elf)
+        wizard = GameCharacterFactory.make(ofType: .wizard)
+        giant = GameCharacterFactory.make(ofType: .giant)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    
+    
+    func testElf() throws {
+        let elf = Elf()
+        
+        XCTAssert(elf.name.contains(try regexElf()))
+        XCTAssert(!elf.name.contains(try regexGiant()))
+        XCTAssert(!elf.name.contains(try regexWizard()))
+        
+        XCTAssert(elf.hitPoints == kStats_Elf.hp)
+        XCTAssert(elf.attackPoints == kStats_Elf.atk)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testWizard() throws {
+        let wizard = Wizard()
+        
+        XCTAssert(!wizard.name.contains(try regexElf()))
+        XCTAssert(!wizard.name.contains(try regexGiant()))
+        XCTAssert(wizard.name.contains(try regexWizard()))
+        
+        XCTAssert(wizard.hitPoints == kStats_Wizard.hp)
+        XCTAssert(wizard.attackPoints == kStats_Wizard.atk)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testGiant() throws {
+        let giant = Giant()
+        
+        XCTAssert(!giant.name.contains(try regexElf()))
+        XCTAssert(giant.name.contains(try regexGiant()))
+        XCTAssert(!giant.name.contains(try regexWizard()))
+        
+        XCTAssert(giant.hitPoints == kStats_Giant.hp)
+        XCTAssert(giant.attackPoints == kStats_Giant.atk)
+    }
+    
+    func testGameFactory() throws {
+        let elfFromFactory = GameCharacterFactory.make(ofType: .elf)
+        XCTAssert(elfFromFactory.name.contains(try regexElf()))
+        XCTAssert(!elfFromFactory.name.contains(try regexGiant()))
+        XCTAssert(!elfFromFactory.name.contains(try regexWizard()))
+        
+        XCTAssert(elfFromFactory.hitPoints == kStats_Elf.hp)
+        XCTAssert(elfFromFactory.attackPoints == kStats_Elf.atk)
+        
+        let wizardFromFactory = GameCharacterFactory.make(ofType: .wizard)
+        XCTAssert(!wizardFromFactory.name.contains(try regexElf()))
+        XCTAssert(!wizardFromFactory.name.contains(try regexGiant()))
+        XCTAssert(wizardFromFactory.name.contains(try regexWizard()))
+        
+        XCTAssert(wizardFromFactory.hitPoints == kStats_Wizard.hp)
+        XCTAssert(wizardFromFactory.attackPoints == kStats_Wizard.atk)
+        
+        
+        let giantFromFactory = GameCharacterFactory.make(ofType: .giant)
+        XCTAssert(!giantFromFactory.name.contains(try regexElf()))
+        XCTAssert(giantFromFactory.name.contains(try regexGiant()))
+        XCTAssert(!giantFromFactory.name.contains(try regexWizard()))
+        
+        XCTAssert(giantFromFactory.hitPoints == kStats_Giant.hp)
+        XCTAssert(giantFromFactory.attackPoints == kStats_Giant.atk)
+    }
+    
+    func testStrikeElf() throws {
+        let elf2 = GameCharacterFactory.make(ofType: .elf)
+        
+        XCTAssert(elf.strike(character: elf2) == true)
+        XCTAssert(elf.strike(character: wizard) == true)
+        XCTAssert(elf.strike(character: giant) == true)
+    }
+    
+    func testStrikeWizard() throws {
+        let wizard2 = GameCharacterFactory.make(ofType: .wizard)
+        
+        XCTAssert(wizard.strike(character: wizard2) == true)
+        XCTAssert(wizard.strike(character: elf) == true)
+        XCTAssert(wizard.strike(character: giant) == false)
+    }
+    
+    func testStrikeGiant() throws {
+        let giant2 = GameCharacterFactory.make(ofType: .giant)
+        
+        XCTAssert(giant.strike(character: giant2) == false)
+        XCTAssert(giant.strike(character: elf) == true)
+        XCTAssert(giant.strike(character: wizard) == false)
     }
 
 }
