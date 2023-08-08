4ba46b059524afd142163537cdd7df438f3e6547
dexter ramos <dexter.ramos@epon.ph>
2023-08-08 14:24:58 +0800

    add encode decode

--- SwiftJog/Walk/EncodeDecode.swift
+++ SwiftJog/Walk/EncodeDecode.swift
@@ -6,3 +6,63 @@
 //
 
 import Foundation
+
+struct Employee: Codable {
+    var name: String
+    var department: String
+    var profile: Profile?
+}
+
+struct Profile: Codable {
+    var language: String
+    
+    enum CodingKeys: CodingKey {
+        case language
+    }
+}
+
+struct EmployeeV2: Codable {
+    var name: String
+    var department: String
+    var profile: Profile?
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "first_name"
+        case department
+        case profile
+    }
+}
+
+struct EmployeeV3 {
+    var name: String
+    var department: String
+    var profile: Profile?
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case department
+        case profile
+    }
+}
+
+extension EmployeeV3: Encodable {
+    func encode(to encoder: Encoder) throws {
+        var values = encoder.container(keyedBy: CodingKeys.self)
+        try values.encode(name, forKey: .name)
+        try values.encode(department, forKey: .department)
+        try values.encodeIfPresent(profile?.language, forKey: .profile)
+    }
+}
+
+extension EmployeeV3: Decodable {
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        name = try values.decode(String.self, forKey: .name)
+        department = try values.decode(String.self, forKey: .department)
+
+        if let language = try values.decodeIfPresent(String.self, forKey: .profile) {
+            
+            profile = Profile(language: language)
+        }
+    }
+}
--- SwiftJogTests/EncodeDecodeTests.swift
+++ SwiftJogTests/EncodeDecodeTests.swift
@@ -20,13 +20,12 @@ class EncodeDecodeTests: XCTestCase {
         
         
         let jsonEncoder = JSONEncoder()
-        let employeeData = try? jsonEncoder.encode(employee)
+        let employeeData = try! jsonEncoder.encode(employee)
         XCTAssertNotNil(employeeData)
         
-
-        let expectedEncodedJson = #"{"name":"Dexter","department":"Tech"}"#
         let encodedJson = String(data: employeeData, encoding: .utf8)!
-        XCTAssertTrue(encodedJson == expectedEncodedJson)
+        XCTAssertTrue(encodedJson.contains(#""name":"Dexter""#))
+        XCTAssertTrue(encodedJson.contains(#""department":"Tech""#))
     }
     
     func testDefaultDecode() {
@@ -34,41 +33,39 @@ class EncodeDecodeTests: XCTestCase {
         let employeeJSON = #"{"name":"Dexter","department":"Tech"}"#
         let employeeData = employeeJSON.data(using: .utf8)!
         
-
         let jsonDecoder = JSONDecoder()
         let employee = try? jsonDecoder.decode(Employee.self, from: employeeData)
         XCTAssertNotNil(employee)
         
-
         let expectedEmployeeObject = Employee(name: "Dexter", department: "Tech")
 
         XCTAssertTrue(employee!.name == expectedEmployeeObject.name)
         XCTAssertTrue(employee!.department == expectedEmployeeObject.department)
     }
-    
+
     /*
      Part 2: Nested data struct
      */
-    
+
     func testDefaultEncode2() {
         let profile = Profile(language: "English")
         let employee = Employee(name: "Dexter", department: "Tech", profile: profile)
-        
-        
-        let jsonEncoder = JSONEncoder()
-        let employeeData = try? jsonEncoder.encode(employee)
-        XCTAssertNotNil(employeeData)
-        
 
-        let expectedEncodedJson = #"{"name":"Dexter","department":"Tech","profile":{language:"English"}}"#
+
+        let jsonEncoder = JSONEncoder()
+        let employeeData = try! jsonEncoder.encode(employee)
+        XCTAssertNotNil(employeeData)
+
         let encodedJson = String(data: employeeData, encoding: .utf8)!
-        XCTAssertTrue(encodedJson == expectedEncodedJson)
+        XCTAssertTrue(encodedJson.contains(#""name":"Dexter""#))
+        XCTAssertTrue(encodedJson.contains(#""department":"Tech""#))
+        XCTAssertTrue(encodedJson.contains(#""profile":{"language":"English"}"#))
     }
-    
+
     func testDefaultDecode2() {
         let employeeJSON = #"{"name":"Dexter","department":"Tech","profile":{"language":"English"}}"#
         let employeeData = employeeJSON.data(using: .utf8)!
-        
+
 
         let jsonDecoder = JSONDecoder()
         let employee = try? jsonDecoder.decode(Employee.self, from: employeeData)
@@ -81,80 +78,125 @@ class EncodeDecodeTests: XCTestCase {
         XCTAssertTrue(employee!.name == expectedEmployeeObject.name)
         XCTAssertTrue(employee!.department == expectedEmployeeObject.department)
     }
-    
+
     /*
      Part 3: different key
      - name key was changed to first_name
-     - create a copy of Employee model and name it EmployeeV3
-     - do not change the structure and names of EmployeeV3 directly
+     - create a copy of Employee model and name it EmployeeV2
+     - do not change the structure and names of EmployeeV2 directly
      */
-    
+
     func testCustomKeyEncode() {
-        let employee = EmployeeV3(name: "Dexter", department: "Tech")
-        
+        let employee = EmployeeV2(name: "Dexter", department: "Tech")
+
         let jsonEncoder = JSONEncoder()
-        let employeeData = jsonEncoder.encode(employee)
-        
+        let employeeData = try! jsonEncoder.encode(employee)
+
         let employeeJson = String(data: employeeData, encoding: .utf8)!
         let expectedEmployeeJson = #"{"first_name":"Dexter","department":"Tech"}"#
-        XCTAssertTrue(employeeJson == expectedEmployeeJson)
-        
+        XCTAssertTrue(employeeJson.contains(#""first_name":"Dexter""#))
+        XCTAssertTrue(employeeJson.contains(#""department":"Tech""#))
+                                            
+
     }
-    
+
     func testCustomKeyDecode() {
         let employeeJson = #"{"first_name":"Dexter","department":"Tech"}"#
         let employeeData = employeeJson.data(using: .utf8)!
-        
+
         let jsonDecoder = JSONDecoder()
-        let employee = jsonDecoder.decode(EmployeeV3.self, from: employeeData)
+        let employee = try! jsonDecoder.decode(EmployeeV2.self, from: employeeData)
         XCTAssertNotNil(employee)
-        
-        let expectedEmployee = EmployeeV3(name: "Dexter", department: "Tech")
+
+        let expectedEmployee = EmployeeV2(name: "Dexter", department: "Tech")
         XCTAssertTrue(employee.name == expectedEmployee.name)
         XCTAssertTrue(employee.department == expectedEmployee.department)
     }
-    
+
 
     /*
-     Part 4: Different data structure
+     Part 4: Different data structure minimally
      - data structure was changed
-        - from: {"name":"Dexter","department":"Tech","profile":{"language":"English"}}
-        - to: {"name":"Dexter","department":"Tech","language":"English"}
+        - from: {"name":"Dexter","department":"Tech","profile":{"profile":"English"}}
+        - to: {"name":"Dexter","department":"Tech","profile":"English"}
+     - copy Employee struct and name it to EmployeeV3
      - make a custom encode(to:) and decode(from:) function
      - do not change the structure of the model
      */
-    
+
     func testCustomEncode() {
         let profile = Profile(language: "English")
-        let employee = EmployeeV2(name: "Dexter", department: "Tech", profile: profile)
-        
+        let employee = EmployeeV3(name: "Dexter", department: "Tech", profile: profile)
+
         let jsonEncoder = JSONEncoder()
-        let employeeData = try? jsonEncoder.encode(employee)
+        let employeeData = try! jsonEncoder.encode(employee)
         XCTAssertNotNil(employeeData)
-        
-        let expectedEncodedJson = #"{"name":"Dexter","department":"Tech","language":"English"}"#
+
         let encodedJson = String(data: employeeData, encoding: .utf8)!
-        XCTAssertTrue(encodedJson == expectedEncodedJson)
+        XCTAssertTrue(encodedJson.contains(#""name":"Dexter""#))
+        XCTAssertTrue(encodedJson.contains(#""department":"Tech""#))
+        XCTAssertTrue(encodedJson.contains(#""profile":"English""#))
     }
     
+    func testCustomEncodeWithMissing() {
+        let profile = Profile(language: "English")
+        let employee = EmployeeV3(name: "Dexter", department: "Tech")
+
+        let jsonEncoder = JSONEncoder()
+        jsonEncoder.outputFormatting = [.sortedKeys]
+        let employeeData = try! jsonEncoder.encode(employee)
+        XCTAssertNotNil(employeeData)
+
+        let encodedJson = String(data: employeeData, encoding: .utf8)!
+        
+        let expectedJson = #"{"department":"Tech","name":"Dexter"}"#
+        XCTAssertTrue(encodedJson == expectedJson, """
+        
+        compared:\t\(encodedJson)
+        expected:\t\t\(expectedJson)
+        """)
+        XCTAssertTrue(encodedJson.contains(#""name":"Dexter""#))
+        XCTAssertTrue(encodedJson.contains(#""department":"Tech""#))
+        XCTAssertTrue(!encodedJson.contains(#""profile":"English""#))
+    }
+
 
     func testCustomDecode() {
-        let employeeJSON = #"{"name":"Dexter","department":"Tech","language":"English"}"#
+        let employeeJSON = #"{"name":"Dexter","department":"Tech","profile":"English"}"#
         let employeeData = employeeJSON.data(using: .utf8)!
-        
+
 
         let jsonDecoder = JSONDecoder()
-        let employee = try? jsonDecoder.decode(EmployeeV2.self, from: employeeData)
+        let employee = try! jsonDecoder.decode(EmployeeV3.self, from: employeeData)
         XCTAssertNotNil(employee)
 
 
         let profile = Profile(language: "English")
         let expectedEmployeeObject = Employee(name: "Dexter", department: "Tech", profile: profile)
 
-        XCTAssertTrue(employee!.name == expectedEmployeeObject.name)
-        XCTAssertTrue(employee!.department == expectedEmployeeObject.department)
+        XCTAssertTrue(employee.name == expectedEmployeeObject.name)
+        XCTAssertTrue(employee.department == expectedEmployeeObject.department)
     }
     
+    func testCustomDecodeWithMissing() {
+        let employeeJSON = #"{"name":"Dexter","department":"Tech"}"#
+        let employeeData = employeeJSON.data(using: .utf8)!
 
+
+        let jsonDecoder = JSONDecoder()
+        let employee = try! jsonDecoder.decode(EmployeeV3.self, from: employeeData)
+        XCTAssertNotNil(employee)
+
+        let profile = Profile(language: "English")
+        let expectedEmployeeObject = Employee(name: "Dexter", department: "Tech", profile: profile)
+        
+        XCTAssertTrue(employee.name == expectedEmployeeObject.name)
+        XCTAssertTrue(employee.department == expectedEmployeeObject.department)
+        XCTAssertNil(employee.profile)
+    }
+
+
+
+    
     
 }
